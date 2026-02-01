e260a01ca315b801448b9498e6b9de21b24d228a
MariusLarilahti <fmala029@edu.xamk.fi>
2026-02-01 07:15:58 +0200

    audioMixerStuff

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using static Unity.VisualScripting.Member;
 using Random = UnityEngine.Random;
 
@@ -23,7 +24,11 @@ public class AudioManager : MonoBehaviour
     private static AudioManager instance;
 
     [SerializeField] private SoundList[] soundList;
-    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioSource audioSourceSFX;
+    [SerializeField] private AudioSource audioSourceMusic;
+
+    [SerializeField] private AudioMixerGroup sfxMixer;
+
 
     [SerializeField] [Range (0, 1f)] private float soundVar;
 
@@ -42,13 +47,18 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    public static void PlayMusic()
+    {
+        instance.audioSourceMusic.Play();
+    }
+
     //Plays sound by SoundType
     public static void PlaySound(SoundType sound, float volume = 1)
     {
         AudioClip[] clips = instance.soundList[(int)sound].Sounds;                                  //List of available clips in given SoundType
         AudioClip clip = clips[Random.Range(0, clips.Length)];                                      //Randomize clip
-        instance.audioSource.pitch = Random.Range(1 - instance.soundVar, 1 + instance.soundVar);    //Randomize pitch
-        instance.audioSource.PlayOneShot(clip, volume);                                             //Play clip
+        instance.audioSourceSFX.pitch = Random.Range(1 - instance.soundVar, 1 + instance.soundVar);    //Randomize pitch
+        instance.audioSourceSFX.PlayOneShot(clip, volume);                                             //Play clip
     }
 
     //Plays sound by SoundType through a temporary GameObject at a given location
@@ -61,7 +71,8 @@ public class AudioManager : MonoBehaviour
         tempGO.transform.position = pos;                                            //Places temporary GameObject at given position
         AudioSource source = tempGO.AddComponent<AudioSource>();                    //Adds AudioSource component to temporary GameObject
 
-        source.pitch = Random.Range(1 - instance.soundVar, 1 + instance.soundVar);  //Randomize pitch
+        source.pitch = Random.Range(1 - instance.soundVar, 1 + instance.soundVar);
+        source.outputAudioMixerGroup = instance.sfxMixer;                                      //Randomize pitch
         source.spatialBlend = 1;                                                    //Enable Spatial Blending
         source.PlayOneShot(clip, volume);                                           //Play clip
 
