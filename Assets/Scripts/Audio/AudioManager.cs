af895cd1b6373ef9ac34fd4c965042ddfabab92d
MariusLarilahti <fmala029@edu.xamk.fi>
2026-02-01 02:12:50 +0200

    vitu hyvä audiomanageri hifistely

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,10 +1,26 @@
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static Unity.VisualScripting.Member;
+using Random = UnityEngine.Random;
 
 
-public class AudioManager : MonoBehaviourSingleton<AudioManager>
+public enum SoundType
 {
+    CLICK,
+    FOOTSTEP,
+    MASK,
+    DEATH,
+    PISTOL,
+    SHOTGUN,
+    RIFLE
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    /*
     [Range(0, 1f)] public float soundVar;
 
     [SerializeField] [Range(0, 1)] private float generalVolume;
@@ -47,19 +63,7 @@ public class AudioManager : MonoBehaviourSingleton<AudioManager>
         source.PlayOneShot(audioClips_Pistol[Random.Range(0, audioClips_Steps.Length)]);
     }
 
-    public void PlaySoundSplatter(Vector3 pos)
-    {
-        GameObject tempGO = new GameObject("TempAudio");
-        tempGO.transform.position = pos;
-        AudioSource source = tempGO.AddComponent<AudioSource>();
-        AudioClip clip = audioClips_Splatters[Random.Range(0, audioClips_Splatters.Length)];
-        source.clip = clip;
-        source.volume = splattersVolume;
-        source.pitch = Random.Range(1 - soundVar, 1 + soundVar);
-        source.spatialBlend = 1;
-        source.Play();
-        Destroy(tempGO, clip.length);
-    }
+    
 
     public void PlaySoundGunshot(int gunID, AudioSource source)
     {
@@ -88,4 +92,62 @@ public class AudioManager : MonoBehaviourSingleton<AudioManager>
     {
         audioMusicSource.Stop();
     }
+    */
+
+
+    private static AudioManager instance;
+
+    [SerializeField] private SoundList[] soundList;
+    [SerializeField] private AudioSource audioSource;
+
+    [SerializeField] [Range (0, 1f)] private float soundVar;
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    //Plays sound by SoundType
+    public static void PlaySound(SoundType sound, float volume = 1)
+    {
+        AudioClip[] clips = instance.soundList[(int)sound].Sounds;                                  //List of available clips in SoundType
+        AudioClip clip = clips[Random.Range(0, clips.Length)];                                      //Randomize clip
+        instance.audioSource.pitch = Random.Range(1 - instance.soundVar, 1 + instance.soundVar);    //Randomize pitch
+        instance.audioSource.PlayOneShot(clip, volume);                                             //Play clip
+    }
+
+    //Plays sound by SoundType through a temporary GameObject at a given location
+    public static void PlaySoundAt(SoundType sound, Vector3 pos, float volume = 1)
+    {
+        AudioClip[] clips = instance.soundList[(int)sound].Sounds;                  //List of available clips in SoundType
+        AudioClip clip = clips[Random.Range(0, clips.Length)];                      //Randomize clip
+
+        GameObject tempGO = new GameObject("TempAudio");                            //Creates temporary GameObject
+        tempGO.transform.position = pos;                                            //Places temporary GameObject at given position
+        AudioSource source = tempGO.AddComponent<AudioSource>();                    //Adds AudioSource component to temporary GameObject
+
+        source.pitch = Random.Range(1 - instance.soundVar, 1 + instance.soundVar);  //Randomize pitch
+        source.spatialBlend = 1;                                                    //Enable Spatial Blending
+        source.Play();                                                              //Play clip
+
+        Destroy(tempGO, clip.length);                                               //Destroys temporary GameObject after clip has played
+    }
+}
+
+[Serializable]
+public struct SoundList
+{
+    public AudioClip[] Sounds { get => sounds; }
+    [SerializeField] private string name;
+    [SerializeField] private AudioClip[] sounds;
 }
